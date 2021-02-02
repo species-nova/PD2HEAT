f4d8f192ec596d46ca68e884c7c09b2f9127ff27
Neslon-Poggers <nanny01023@gmail.com>
2021-02-02 17:54:40 -0500

    Update groupaitweakdata.lua

--- lua/sc/tweak_data/groupaitweakdata.lua
+++ lua/sc/tweak_data/groupaitweakdata.lua
@@ -8560,7 +8560,7 @@ function GroupAITweakData:_init_unit_categories(difficulty_index)
 		self.unit_categories.SKULL_tank = {
 			unit_types = {
 				america = {
-					Idstring("units/payday2/characters/	/ene_bulldozer_3")								
+					Idstring("units/payday2/characters/ene_bulldozer_3/ene_bulldozer_3")								
 				},
 				russia = {
 					Idstring("units/pd2_dlc_mad/characters/ene_akan_fbi_tank_rpk_lmg/ene_akan_fbi_tank_rpk_lmg")								
