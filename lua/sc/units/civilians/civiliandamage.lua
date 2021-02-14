7d9502a3fbc54c38c6bf8ce437c4fb6ca87a1595
Fuglore <44823551+fuglore@users.noreply.github.com>
2021-02-14 18:30:19 -0300

    fixed silly civdamage crash

--- lua/sc/units/civilians/civiliandamage.lua
+++ lua/sc/units/civilians/civiliandamage.lua
@@ -19,8 +19,10 @@ function CivilianDamage:damage_fire(attack_data)
 	if not managers.groupai:state():whisper_mode() then
 		if managers.player:has_category_upgrade("player", "civ_harmless_bullets") and self.no_intimidation_by_dmg and not self:no_intimidation_by_dmg() and (not self._survive_shot_t or self._survive_shot_t < TimerManager:game():time()) then
 			self._survive_shot_t = TimerManager:game():time() + 2.5
-
-			self._unit:brain():on_intimidated(1, attack_data.attacker_unit)
+			
+			if attack_data.attacker_unit and alive(attack_data.attacker_unit) then
+				self._unit:brain():on_intimidated(1, attack_data.attacker_unit)
+			end
 
 			return
 		end
