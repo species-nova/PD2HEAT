36acf7190827d297202a4a6c02caea5c13bde0dd
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-15 00:26:15 -0300

    Fixed some severe issues with vehicles and Team AI
    Cancelling or failing the SO to enter a vehicle caused the team AI to never be kicked or tased until they actually got inside one. This also prevented them from grabbing bags thrown at them.

---
+++ lua/sc/units/vehicles/vehicledrivingext.lua
@@ -0,0 +1,47 @@
+function VehicleDrivingExt:on_drive_SO_failed(seat, unit)
+	local so_data = seat.drive_SO_data
+
+	if not so_data then
+		return
+	end
+
+	if unit ~= so_data.unit then
+		return
+	end
+
+	if alive(unit) then
+		local mov_ext = unit:movement()
+
+		mov_ext.vehicle_unit = nil
+		mov_ext.vehicle_seat = nil
+	end
+
+	seat.drive_SO_data = nil
+
+	self:_create_seat_SO(seat)
+end
+
+function VehicleDrivingExt:_unregister_drive_SO(seat)
+	local so_data = seat.drive_SO_data
+
+	if not so_data then
+		return
+	end
+
+	seat.drive_SO_data = nil
+
+	if SO_data.SO_registered then
+		managers.groupai:state():remove_special_objective(SO_data.SO_id)
+	end
+
+	local bot_unit = SO_data.unit
+
+	if alive(bot_unit) then
+		local mov_ext = bot_unit:movement()
+
+		mov_ext.vehicle_unit = nil
+		mov_ext.vehicle_seat = nil
+
+		bot_unit:brain():set_objective(nil)
+	end
+end
