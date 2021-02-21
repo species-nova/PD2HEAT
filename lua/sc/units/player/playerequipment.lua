261d7d9a21c9af15bff70508fbaf0e7b7d4997f8
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-21 11:13:29 -0300

    Fixed some issues with valid_shape_placement()
    -Fixed ray_type "equipment_placement" being missing for some reason.
    
    -Will no longer leave disabled dummy units that stick around forever, instead, the dummy units are set to be visible or not accordingly (they're automatically destroyed when finishing or cancelling placement). Before, every time you'd see the dummy unit disappear, it still existed, but if you were to get a valid placement without cancelling or (unsuccessfully) finishing the action, a new one would be spawned, leaving the previous one disabled (no rendering or updating) while still existing forever.
    
    -Also optimized the file and made it more user-friendly.

--- lua/sc/units/player/playerequipment.lua
+++ lua/sc/units/player/playerequipment.lua
@@ -1,72 +1,112 @@
-local Net = _G.LuaNetworking
+local math_up = math.UP
+local math_dot = math.dot
+
+local alive_g = alive
+local world_g = World
+
+local idstr_func = Idstring
+local body_idstr = idstr_func("body")
+
+local rot_mods = {
+	armor_kit = 180,
+	bodybags_bag = -90
+}
+
+local custom_find_params = {
+	ammo_bag = {
+		20,
+		21,
+		12
+	},
+	doctor_bag = {
+		22,
+		28,
+		15
+	}
+}
 
 function PlayerEquipment:valid_shape_placement(equipment_id, equipment_data)
-	local from = self._unit:movement():m_head_pos()
-	local to = from + self._unit:movement():m_head_rot():y() * 220
-	local ray = self._unit:raycast("ray", from, to, "slot_mask", managers.slot:get_mask("trip_mine_placeables"), "ignore_unit", {})
-	local valid = ray and true or false
-	if ray then
-		local pos = ray.position
-		local rot = self._unit:movement():m_head_rot()
-		rot = Rotation(rot:yaw(), 0, 0)
+	local unit = self._unit
+	local mov_ext = unit:movement()
 
-		if equipment_id == "armor_kit" then				
-			rot = Rotation(rot:yaw()+180, 0, 0)
-		elseif equipment_id == "bodybags_bag" then 
-			rot = Rotation(rot:yaw()-90, 0, 0)
-		end
-		
-		if not alive(self._dummy_unit) then
-			self._dummy_unit = World:spawn_unit(Idstring(equipment_data.dummy_unit), pos, rot)
-			self:_disable_contour(self._dummy_unit)
-		end
-		self._dummy_unit:set_position(pos)
-		self._dummy_unit:set_rotation(rot)
-		valid = valid and math.dot(ray.normal, math.UP) > 0.25
-		local find_start_pos, find_end_pos, find_radius
-		if equipment_id == "ammo_bag" then
-			find_start_pos = pos + math.UP * 20
-			find_end_pos = pos + math.UP * 21
-			find_radius = 12
-		elseif equipment_id == "doctor_bag" then
-			find_start_pos = pos + math.UP * 22
-			find_end_pos = pos + math.UP * 28
-			find_radius = 15
-		else
-			find_start_pos = pos + math.UP * 30
-			find_end_pos = pos + math.UP * 40
-			find_radius = 17
-		end
-		local bodies = self._dummy_unit:find_bodies("intersect", "capsule", find_start_pos, find_end_pos, find_radius, managers.slot:get_mask("trip_mine_placeables") + 14 + 25)
-		for _, body in ipairs(bodies) do
-			if body:unit() ~= self._dummy_unit and body:has_ray_type(Idstring("body")) then
-				valid = false
+	local from = mov_ext:m_head_pos()
+	local head_rot = mov_ext:m_head_rot()
+	local to = from + head_rot:y() * 220
+
+	local ray = unit:raycast("ray", from, to, "slot_mask", managers.slot:get_mask("trip_mine_placeables"), "ray_type", "equipment_placement")
+	local valid = ray and true or false
+
+	local dummy_unit = self._dummy_unit
+	local dummy_pos, dummy_rot = nil
+
+	if ray then
+		dummy_pos = ray.position
+
+		local rot_mod = rot_mods[equipment_id]
+		dummy_rot = rot_mod and Rotation(head_rot:yaw() + rot_mod, 0, 0) or Rotation(head_rot:yaw(), 0, 0)
+
+		valid = math_dot(ray.normal, math_up) > 0.25
+
+		if valid then
+			if alive_g(dummy_unit) then
+				dummy_unit:set_position(dummy_pos)
+				dummy_unit:set_rotation(dummy_rot)
 			else
+				dummy_unit = world_g:spawn_unit(idstr_func(equipment_data.dummy_unit), dummy_pos, dummy_rot)
+
+				self:_disable_contour(dummy_unit)
+			end
+
+			local find_params = custom_find_params[equipment_id] or {30, 40, 17}
+			local find_start_pos = dummy_pos + math_up * find_params[1]
+			local find_end_pos = dummy_pos + math_up * find_params[2]
+			local find_radius = find_params[3]
+
+			local bodies = dummy_unit:find_bodies("intersect", "capsule", find_start_pos, find_end_pos, find_radius, managers.slot:get_mask("trip_mine_placeables") + 14 + 25)
+
+			for i = 1, #bodies do
+				local body = bodies[i]
+
+				if body:has_ray_type(body_idstr) then
+					valid = false
+
+					break
+				end
 			end
 		end
 	end
-	if alive(self._dummy_unit) then
-		self._dummy_unit:set_enabled(valid)
+
+	if alive_g(dummy_unit) then
+		dummy_unit:set_visible(valid)
 	end
+
+	self._dummy_unit = dummy_unit
+
 	return valid and ray
 end
 
---Armour Bag
+local Net = _G.LuaNetworking
+
+--Grenade Case
 function PlayerEquipment:use_armor_kit()
 	local ray = self:valid_shape_placement("grenade_crate")
+
 	if ray then
-		local pos = ray.position
-		local rot = self._unit:movement():m_head_rot()
-		rot = Rotation(rot:yaw()+180, 0, 0)
 		PlayerStandard.say_line(self, "s01x_plu")
 		managers.statistics:use_armor_bag()
+
+		local pos = ray.position
+		local rot = Rotation(self._unit:movement():m_head_rot():yaw() + 180, 0, 0)
 		local amount_upgrade_lvl = 0
+
 		if Network:is_client() then
 			Net:SendToPeer(1, "PlaceGrenadeCrate", tostring(pos).."|"..tostring(rot).."|"..tostring(amount_upgrade_lvl))
 		else
-			local unit = GrenadeCrateBase.spawn(pos, rot, amount_upgrade_lvl, managers.network:session():local_peer():id())
+			GrenadeCrateBase.spawn(pos, rot, amount_upgrade_lvl, managers.network:session():local_peer():id())
 		end
+
 		return true
 	end
+
 	return false
 end
