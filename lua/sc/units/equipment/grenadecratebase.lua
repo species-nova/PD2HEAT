fd0f2270d8bca88f02bc75e6b5097d9067a63109
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-23 04:26:35 -0300

    Fucking stupid hacky shit implementation of things

--- lua/sc/units/equipment/grenadecratebase.lua
+++ lua/sc/units/equipment/grenadecratebase.lua
@@ -20,8 +20,11 @@ function GrenadeCrateBase:take_grenade(unit)
 		local register_grenade_func = pl_manager.register_grenade
 		local my_unit = self._unit
 
-		for i = 1, taken_amount do
-			send_f(session, "sync_unit_event_id_16", my_unit, "base", 1)
+		send_f(session, "sync_unit_event_id_16", my_unit, "base", 1)
+		register_grenade_func(pl_manager, local_peer_id)
+
+		for i = 1, taken_amount - 1 do
+			send_f(session, "sync_unit_event_id_16", my_unit, "base", 3)
 
 			register_grenade_func(pl_manager, local_peer_id)
 		end
@@ -36,4 +39,17 @@ function GrenadeCrateBase:take_grenade(unit)
 	self:_set_visual_stage()
 
 	return taken_amount
+end
+
+local sync_net_event_original = GrenadeCrateBase.sync_net_event
+function GrenadeCrateBase:sync_net_event(event_id, peer)
+	if event_id == 3 then
+		if peer then
+			managers.player:register_grenade(peer:id())
+		end
+
+		return
+	end
+
+	sync_net_event_original(self, event_id, peer)
 end
