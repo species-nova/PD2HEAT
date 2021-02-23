067036cd286eb59d2d48b1000e46ab679ebfc8ea
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-23 02:57:42 -0300

    Non-hacky grenade registering (needs testing)

--- lua/sc/units/equipment/grenadecratebase.lua
+++ lua/sc/units/equipment/grenadecratebase.lua
@@ -3,18 +3,30 @@ function GrenadeCrateBase:take_grenade(unit)
 		return
 	end
 
-	local can_take_grenade = self:_can_take_grenade() and 1 or 0
+	local taken_amount = 0
 
-	if can_take_grenade == 1 then
+	if self:_can_take_grenade() then
 		unit:sound():play("pickup_ammo")
-		--Guarantees a fill up
-		managers.player:add_grenade_amount(21)
-		managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "base", 1)
-		managers.player:register_grenade(managers.network:session():local_peer():id())
+
+		local session = managers.network:session()
+		local local_peer_id = session:local_peer():id()
+		local pl_manager = managers.player
+
+		taken_amount = pl_manager:get_max_grenades_by_peer_id(local_peer_id) - pl_manager:get_grenade_amount(local_peer_id)
+
+		pl_manager:add_grenade_amount(taken)
+
+		local send_f = session.send_to_peers_synched
+		local register_grenade_func = pl_manager.register_grenade
+		local my_unit = self._unit
+
+		for i = 1, taken do
+			send_f(session, "sync_unit_event_id_16", my_unit, "base", 1)
+
+			register_grenade_func(pl_manager, local_peer_id)
+		end
 
 		self._grenade_amount = self._grenade_amount - 1
-
-		print("Took " .. 1 .. " grenades, " .. self._grenade_amount .. " left")
 	end
 
 	if self._grenade_amount <= 0 then
@@ -23,5 +35,5 @@ function GrenadeCrateBase:take_grenade(unit)
 
 	self:_set_visual_stage()
 
-	return can_take_grenade
+	return taken_amount
 end
