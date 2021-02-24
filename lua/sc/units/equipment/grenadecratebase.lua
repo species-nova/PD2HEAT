13ca83a14f4c16f4f070e8a165c8a9fd940222de
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-23 22:17:44 -0300

    Grenade cases now "despawn" when emptied
    Yes, "despawn". There's two separate units on the ones you can normally buy as an asset. The cosmetic one will not despawn, but this is still infinitely better than leaving the unit you interact with there forever, just with interactions disabled (and constant updating).
    
    The game will also ensure that the case has a dynamic body before enabling frame by frame updating on the extension.
    
    Additionally, if the case becomes dynamic, the extension's updating (frame by frame) will be disabled, instead of keeping it enabled and having the function return nothing immediately. This only affects the server normally, and doing this helps alleviating things there.

--- lua/sc/units/equipment/grenadecratebase.lua
+++ lua/sc/units/equipment/grenadecratebase.lua
@@ -1,3 +1,56 @@
+local ids_base = Idstring("base")
+
+function GrenadeCrateBase:setup()
+	self._grenade_amount = self._max_grenade_amount or tweak_data.upgrades.grenade_crate_base
+	self._empty = false
+
+	self:_set_visual_stage()
+
+	if not Network:is_server() or not self._is_attachable then
+		return
+	end
+
+	local my_unit = self._unit
+
+	if not alive(my_unit:body("dynamic")) then
+		return
+	end
+
+	local cur_pos = my_unit:position()
+	local z_rot = my_unit:rotation():z()
+
+	local from_pos = cur_pos + z_rot * 10
+	local to_pos = cur_pos + z_rot * -10
+	local ray = my_unit:raycast("ray", from_pos, to_pos, "slot_mask", managers.slot:get_mask("world_geometry"))
+
+	if ray then
+		self._attached_data = {
+			body = ray.body,
+			position = ray.body:position(),
+			rotation = ray.body:rotation(),
+			index = 1,
+			max_index = 3
+		}
+
+		my_unit:set_extension_update_enabled(ids_base, true)
+	end
+end
+
+function GrenadeCrateBase:server_set_dynamic()
+	local my_unit = self._unit
+	my_unit:set_extension_update_enabled(ids_base, false)
+
+	self:_set_dynamic()
+
+	local session = managers.network:session()
+
+	if not session then
+		return
+	end
+
+	session:send_to_peers_synched("sync_unit_event_id_16", my_unit, "base", 2)
+end
+
 function GrenadeCrateBase:take_grenade(unit)
 	if self._empty then
 		return
@@ -30,13 +83,13 @@ function GrenadeCrateBase:take_grenade(unit)
 		end
 
 		self._grenade_amount = self._grenade_amount - 1
-	end
 
-	if self._grenade_amount <= 0 then
-		self:_set_empty()
-	end
+		self:_set_visual_stage()
 
-	self:_set_visual_stage()
+		if self._grenade_amount <= 0 then
+			self:_set_empty()
+		end
+	end
 
 	return taken_amount
 end
@@ -52,4 +105,17 @@ function GrenadeCrateBase:sync_net_event(event_id, peer)
 	end
 
 	sync_net_event_original(self, event_id, peer)
+end
+
+function GrenadeCrateBase:_set_empty()
+	self._empty = true
+
+	local my_unit = self._unit
+
+	if Network:is_server() then
+		my_unit:set_slot(0)
+	else
+		my_unit:set_visible(false)
+		my_unit:interaction():set_active(false)
+	end
 end
