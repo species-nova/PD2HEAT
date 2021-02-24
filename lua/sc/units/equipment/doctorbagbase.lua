9646690832674485cc2c3af77fd8f55c37c33a2b
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-23 22:29:03 -0300

    Doctor bag fixes
    -Clients make the unit invisible and disable the interaction prompt instead of setting the slot to 0. Mainly here as future-proofing in case doctor bags are made to work like ammo bags (since their code is identical for the most part), to prevent an issue with floating point values when a client uses the bag, syncs the amount taken, and it remains on the server with 0.000000000000001 or so. Since clients have no authority to removed networked units, they stop despawning things completely due to the way that system is handled.
    
    -"player_refill_doctorbag" global event is only called by the server, which now happens on synced instances. Pretty sure this is just used on the Basic Heists, but whatever, consistency and future-proofing.
    
    -If the bag becomes dynamic, the extension's updating (frame by frame) will be disabled, instead of keeping it enabled and having the function return nothing immediately. This only affects the server normally, and doing this helps alleviating things there.

--- lua/sc/units/equipment/doctorbagbase.lua
+++ lua/sc/units/equipment/doctorbagbase.lua
@@ -1,8 +1,27 @@
+local math_ceil = math.ceil
+
+local ids_base = Idstring("base")
+
+function DoctorBagBase:server_set_dynamic()
+	local my_unit = self._unit
+	my_unit:set_extension_update_enabled(ids_base, false)
+
+	self:_set_dynamic()
+
+	local session = managers.network:session()
+
+	if not session then
+		return
+	end
+
+	session:send_to_peers_synched("sync_unit_event_id_16", my_unit, "base", 1)
+end
+
 function DoctorBagBase:take(unit)
 	if self._empty then
 		return
 	end
-	
+
 	--Remove first aid damage reduction.
 
 	local taken = self:_take(unit)
@@ -10,7 +29,10 @@ function DoctorBagBase:take(unit)
 	if taken > 0 then
 		unit:sound():play("pickup_ammo")
 		managers.network:session():send_to_peers_synched("sync_doctor_bag_taken", self._unit, taken)
-		managers.mission:call_global_event("player_refill_doctorbag")
+
+		if Network:is_server() then
+			managers.mission:call_global_event("player_refill_doctorbag")
+		end
 	end
 
 	if self._amount <= 0 then
@@ -24,14 +46,45 @@ end
 
 --Make Doctor's Bags only have up to 3 blood bags.
 function DoctorBagBase:_set_visual_stage()
+	local unit_dmg = self._unit:damage()
+
+	if not unit_dmg then
+		return
+	end
+
 	local percentage = self._amount / self._max_amount
 
-	if self._unit:damage() then
-		--*3 instead of *4
-		local state = "state_" .. math.ceil(percentage * 3)
+	--*3 instead of *4
+	local state = "state_" .. math_ceil(percentage * 3)
 
-		if self._unit:damage():has_sequence(state) then
-			self._unit:damage():run_sequence_simple(state)
-		end
+	if unit_dmg:has_sequence(state) then
+		unit_dmg:run_sequence_simple(state)
+	end
+end
+
+function DoctorBagBase:_set_empty()
+	self._empty = true
+
+	local my_unit = self._unit
+
+	if Network:is_server() then
+		my_unit:set_slot(0)
+	else
+		my_unit:set_visible(false)
+		my_unit:interaction():set_active(false)
+	end
+end
+
+function DoctorBagBase:sync_taken(amount)
+	self._amount = self._amount - amount
+
+	if Network:is_server() then
+		managers.mission:call_global_event("player_refill_doctorbag")
+	end
+
+	if self._amount <= 0 then
+		self:_set_empty()
+	else
+		self:_set_visual_stage()
 	end
 end
