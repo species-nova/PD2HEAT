1c097a897b9ec15db0119d2e4bb917f0d75a8b5d
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-23 22:00:21 -0300

    Body bag cases now despawn when emptied
    Along with other changes, such as:
    
    -"player_refill_bodybagsbag" global event is only called by the server, which now happens on synced instances. Pretty sure this is just used on the Basic Heists, but whatever, consistency and future-proofing.
    
    -No longer adds 21 body bags to "guarantee a fill up", but rather refills based on the amount the player is allowed to carry minus the current ones they have.
    
    -If the case becomes dynamic, the extension's updating (frame by frame) will be disabled, instead of keeping it enabled and having the function return nothing immediately. This only affects the server normally, and doing this helps alleviating things there.

--- lua/sc/units/equipment/bodybagsbagbase.lua
+++ lua/sc/units/equipment/bodybagsbagbase.lua
@@ -1,26 +1,76 @@
+local ids_base = Idstring("base")
+
+function BodyBagsBagBase:server_set_dynamic()
+	local my_unit = self._unit
+	my_unit:set_extension_update_enabled(ids_base, false)
+
+	self:_set_dynamic()
+
+	local session = managers.network:session()
+
+	if not session then
+		return
+	end
+
+	session:send_to_peers_synched("sync_unit_event_id_16", my_unit, "base", 2)
+end
+
 function BodyBagsBagBase:take_bodybag(unit)
 	if self._empty then
 		return
 	end
 
-	local can_take_bodybag = self:_can_take_bodybag() and 1 or 0
+	local taken_amount = 0
 
-	if can_take_bodybag == 1 then
+	if self:_can_take_bodybag() then
 		unit:sound():play("pickup_ammo")
-		--Guarantees a fill up
-		managers.player:add_body_bags_amount(21)
+
+		local pl_manager = managers.player
+
+		taken_amount = pl_manager:max_body_bags() - pl_manager:get_body_bags_amount()
+		pl_manager:add_body_bags_amount(taken_amount)
+
 		managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "base", 1)
 
 		self._bodybag_amount = self._bodybag_amount - 1
 
-		managers.mission:call_global_event("player_refill_bodybagsbag")
+		if Network:is_server() then
+			managers.mission:call_global_event("player_refill_bodybagsbag")
+		end
+
+		self:_set_visual_stage()
+
+		if self._bodybag_amount <= 0 then
+			self:_set_empty()
+		end
 	end
 
-	if self._bodybag_amount <= 0 then
-		self:_set_empty()
+	return taken_amount
+end
+
+function BodyBagsBagBase:sync_bodybag_taken(amount)
+	self._bodybag_amount = self._bodybag_amount - amount
+
+	if Network:is_server() then
+		managers.mission:call_global_event("player_refill_bodybagsbag")
 	end
 
 	self:_set_visual_stage()
 
-	return can_take_bodybag
+	if self._bodybag_amount <= 0 then
+		self:_set_empty()
+	end
+end
+
+function BodyBagsBagBase:_set_empty()
+	self._empty = true
+
+	local my_unit = self._unit
+
+	if Network:is_server() then
+		my_unit:set_slot(0)
+	else
+		my_unit:set_visible(false)
+		my_unit:interaction():set_active(false)
+	end
 end
