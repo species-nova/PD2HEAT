25e20af3e4a73059843b93b6e4e8b408755681e3
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2021-02-26 09:43:49 -0300

    Fixed client crash when bots spawn

--- lua/sc/units/player_team/huskteamaimovement.lua
+++ lua/sc/units/player_team/huskteamaimovement.lua
@@ -15,3 +15,25 @@ function HuskTeamAIMovement:_switch_to_not_cool(...)
 		self._unit:inventory():_ensure_weapon_visibility()
 	end
 end
+
+function HuskTeamAIMovement:add_weapons()
+	local weapon = self._ext_base:default_weapon_name("primary")
+
+	if type(weapon) == "table" then
+		weapon = weapon[1]
+	end
+
+	if weapon then
+		self._unit:inventory():add_unit_by_factory_name(weapon, false, false, nil, "")
+	end
+
+	local sec_weapon = self._ext_base:default_weapon_name("secondary")
+
+	if type(sec_weapon) == "table" then
+		sec_weapon = sec_weapon[1]
+	end
+
+	if sec_weapon then
+		self._unit:inventory():add_unit_by_factory_name(sec_weapon, false, false, nil, "")
+	end
+end
