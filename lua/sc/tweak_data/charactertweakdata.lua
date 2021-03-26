d94702035fa996d5870720c1ec1166a12b60b74c
Ravicale <epicnetmail@gmail.com>
2021-03-26 18:01:18 -0500

    Sniper head health decreased to 20 (from 36).
    From playtesting with SC in RM, snipers felt a bit too tanky with certain guns (mostly SMGs).
    	Sniper health is lower here than in RM, but not by enough to make a massive diff.
    	Cutting them down to keep things feeling alright. 20 head health should be enough to stop most weird unintuitive shotgun falloff minimum breakpoint bullshit from happening.

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -1351,7 +1351,7 @@ function CharacterTweakData:_init_sniper(presets)
 	self.sniper.experience = {}
 	self.sniper.weapon = presets.weapon.sniper
 	self.sniper.detection = presets.detection.sniper
-	self.sniper.HEALTH_INIT = 10.8
+	self.sniper.HEALTH_INIT = 6
 	self.sniper.headshot_dmg_mul = normal_headshot
 	self.sniper.damage.hurt_severity = presets.hurt_severities.no_hurts
 	self.sniper.allowed_poses = {stand = true}
