080fbf0aa4ee63cdc3e32d6f93bef9aec2127fdb
Ravicale <epicnetmail@gmail.com>
2022-01-27 20:49:14 -0600

    Fix audio bug caused by holdover vanilla code.
    Sometimes the second gun wouldn't play its firing sound.

--- lua/sc/units/weapons/akimboweaponbase.lua
+++ lua/sc/units/weapons/akimboweaponbase.lua
@@ -69,10 +69,7 @@ function AkimboWeaponBase:_fire_second_gun(...)
 		local fired = second_gun_base.super.fire(second_gun_base, ...)
 
 		if fired then
-			if self._fire_second_sound then
-				self._fire_second_sound = false
-				self._second_gun:base():_fire_sound()
-			end
+			self._second_gun:base():_fire_sound()
 
 			managers.hud:set_ammo_amount(self:selection_index(), self:ammo_info())
 			
