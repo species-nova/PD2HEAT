aa925c948c709e15bbbe8fe95fe123623663776c
Ravicale <epicnetmail@gmail.com>
2022-04-01 22:00:26 -0500

    Optimized crossbow anim code, string updates now respond to snatch.

--- lua/sc/units/weapons/bowweaponbase.lua
+++ lua/sc/units/weapons/bowweaponbase.lua
@@ -3,33 +3,52 @@
 --appear as if it's already been drawn. This code resolves the issue by finding the 'barrel'
 --component on affected crossbows and playing the reload up to the point where the string should end up.
 
+function CrossbowWeaponBase:_update_stats_values(...)
+	self.super._update_stats_values(self, ...)
+
+	--Cache a reference to the barrel part, if it can be used for crossbow string movement.
+	self._animated_barrel_part = nil
+	local unit_anim = self:_get_tweak_data_weapon_animation("reload")
+	for part_id, part in pairs(self._parts) do
+		local part_tweak = tweak_data.weapon.factory.parts[part_id]
+		if part.unit and part.animations and part.animations[unit_anim] and part_tweak and part_tweak.type == "barrel" then
+			self._animated_barrel_part = part
+			break
+		end
+	end
+end
+
 function CrossbowWeaponBase:on_enabled(...)
 	self.super.on_enabled(self, ...)
-	self:fake_crossbow_string_movement(instant)
+	self:update_crossbow_string(instant)
 end
 
 function CrossbowWeaponBase:fire(...)
 	local result = self.super.fire(self, ...)
-	self:fake_crossbow_string_movement()
+	self:update_crossbow_string()
 	return result
 end
 
-function CrossbowWeaponBase:fake_crossbow_string_movement(instant)
-	local string_time = self:weapon_tweak_data().crossbow_string_time
-	
-	if string_time and self:clip_empty() then
-		local unit_anim = self:_get_tweak_data_weapon_animation("reload")
-		for part_id, data in pairs(self._parts) do
-			local part_tweak = tweak_data.weapon.factory.parts[part_id]
-			if data.unit and data.animations and data.animations[unit_anim] and part_tweak and part_tweak.type == "barrel" then
-				local anim_name = data.animations[unit_anim]
-				local ids_anim_name = Idstring(anim_name)
-				local length = data.unit:anim_length(ids_anim_name)
+function CrossbowWeaponBase:set_ammo_remaining_in_clip(...)
+	self.super.set_ammo_remaining_in_clip(self, ...)
+	self:update_crossbow_string()
+end
 
-				data.unit:anim_stop(ids_anim_name)
-				data.unit:anim_play_to(ids_anim_name, string_time, instant and 10 or self._fire_rate_multiplier)
-				return
-			end
-		end
+function CrossbowWeaponBase:update_crossbow_string(instant)
+	local string_time = self:weapon_tweak_data().crossbow_string_time
+	if not string_time or not self._animated_barrel_part then 
+		return
+	end
+	
+	local unit_anim = self:_get_tweak_data_weapon_animation("reload")
+	local anim_name = self._animated_barrel_part.animations[unit_anim]
+	local part_unit = self._animated_barrel_part.unit
+	local ids_anim_name = Idstring(anim_name)
+	local length = part_unit:anim_length(ids_anim_name)
+	local speed = instant and 10 or self._fire_rate_multiplier
+
+	part_unit:anim_stop(ids_anim_name)
+	if self:clip_empty() then
+		part_unit:anim_play_to(ids_anim_name, string_time, speed)
 	end
 end
