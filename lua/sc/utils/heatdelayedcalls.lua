a8dfac7b1874f3e145674b2e08fa6ec12d7e4b07
Ravicale <epicnetmail@gmail.com>
2022-05-24 20:10:25 -0500

    Some metatable jank to solve some extremely obscure bugs.

---
+++ lua/sc/utils/heatdelayedcalls.lua
@@ -0,0 +1,27 @@
+--Wrapper over DelayedCalls:Add to resolve delayed call name collisions in an automated manner.
+
+--Metatable to track number of delayed calls of a given type, and cache delayed calls strings to reduce string concatenations.
+local call_name_cache = {}
+local call_name_cache_meta = {
+	__index = function(cache, func)
+		cache[func] = {count = 0}
+		setmetatable(cache[func], {
+			__index = function(t, count)
+				t[count] = func .. count
+				return t[count]
+			end
+		})
+		return cache[func]
+	end
+}
+setmetatable(call_name_cache, call_name_cache_meta)
+
+--Call this in the same way as DelayedCalls:Add.
+function AddHeatDelayedCall(name, delay, lambda)
+	local count = call_name_cache[name].count 
+	call_name_cache[name].count = count + 1
+	DelayedCalls:Add(call_name_cache[name][count], delay, function()
+		lambda()
+		call_name_cache[name].count = count - 1
+	end)
+end
